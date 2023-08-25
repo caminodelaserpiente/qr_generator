f5793e058c01b9e7de2b6dc65bcdb2bd9a41e13e
蛇道 <caminodelaserpiente.py@gmail.com>
2023-08-24 22:56:10 -0600

    [ADD] Launcher Application

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Author: 蛇道
+Url: https://github.com/caminodelaserpiente
+"""
+
+
+from qr_generator import qr_generator
+
+
+def main():
+    qr_generator()
+
+
+if __name__ == "__main__":
+    main()
